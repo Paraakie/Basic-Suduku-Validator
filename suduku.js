9fd7f72ba042f4a1a8274cf4694477a4a0cb3ccb
Paraakie <sven.m.gerhards@gmail.com>
2020-10-28 22:31:53 +1300

    Update suduku.js

--- suduku.js
+++ suduku.js
@@ -2,3 +2,16 @@
 
 
 //Maybe use React?
+
+
+var sudukuField =   { 
+                        1: 'array1',
+                        2: 'array2',
+                        3: 'array3',
+                        4: 'array4',
+                        5: 'array5',
+                        6: 'array6',
+                        7: 'array7',
+                        8: 'array8',
+                        9: 'array9'
+                    }
