63e04845ece464e1fa3cafa029b462049926b32c
Sven Gerhards <sven.m.gerhards@gmail.com>
2020-11-09 14:02:13 +1300

    Update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -21,16 +21,15 @@ var prev = null;
 sudokuField.sort();
 for(var i = 0; i < size; i++){
 	for(var j = 0; j < size; j++) {
-      if ( sudokuField[i][j] !== prev) {
-            // No duplicate
-        		console.log('No Duplicate')
-      } else {
-        //Found duplicate
-        		console.log('Duplicate');
-            
-      }
+		if ( sudokuField[i][j] !== prev) { 
+			// No duplicate
+	      		console.log('No Duplicate')
+      		} else {
+        		//Found duplicate
+        		console.log('Duplicate')      
+		}
   		prev = sudokuField[i][j];
-  }
+  	}
 }
 
  
